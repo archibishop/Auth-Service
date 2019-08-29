c8f14ba19f54e48939ab4a257b9076cda271297b
Wagubi Brian <wagubib@gmail.com>
2019-08-29 14:41:05 +0300

    Refactor code base

--- api/helpers/sendEmail.js
+++ api/helpers/sendEmail.js
@@ -1,10 +1,16 @@
 /* eslint-disable no-console */
 import nodemailer from 'nodemailer';
 
-function sendEmail(mailOptions) {
-  const email = process.env.EMAIL;
+function sendEmail(email, id) {
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: 'Account Verification',
+    html: `<p>Please verify your account before you can use it Click <a href='${process.env.HOST}/api/v1/users/activate/${id}'>Link<a/></p>`,
+  };
+  const emailAddr = process.env.EMAIL;
   const pass = process.env.PASS;
-  const transport = nodemailer.createTransport({ service: 'gmail', auth: { user: email, pass } });
+  const transport = nodemailer.createTransport({ service: 'gmail', auth: { user: emailAddr, pass } });
   transport.sendMail(mailOptions, (err, info) => {
     if (err) {
       console.log(err);
--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import AuthController from '../controllers/authController';
+import ValidationMiddleware from '../middleware/validationMiddleware';
 
 const router = express.Router();
 
@@ -7,15 +8,15 @@ const router = express.Router();
 router.get('/api/v1/users', AuthController.getUsers);
 
 // Create users
-router.post('/api/v1/users', AuthController.createUser);
+router.post('/api/v1/users', ValidationMiddleware, AuthController.createUser);
 
 // Activate user account
 router.get('/api/v1/users/activate/:id', AuthController.activateAccount);
 
 // Login user
-router.post('/api/v1/users/login', AuthController.loginUser);
+router.post('/api/v1/users/login', ValidationMiddleware, AuthController.loginUser);
 
 // Reset password
-router.post('/api/v1/users/reset-password', AuthController.resetPassword);
+router.post('/api/v1/users/reset-password', ValidationMiddleware, AuthController.resetPassword);
 
 export default router;
