bfd3ae43a48b5d184abb238f3ed5b97453e76fbd
Wagubi Brian <wagubib@gmail.com>
2019-11-06 11:04:23 +0300

    Refactor and make changes to docker compose.

--- api/repository/repository.js
+++ api/repository/repository.js
@@ -1,69 +1,58 @@
 import db from './db';
 
 const repository = () => {
-    const createUserRepo = async (user) => {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const response = await db.createUser(user)
-                resolve(response)
-            } catch (err) {
-                resolve(err)
-            }
-        });
+  const createUserRepo = async (user) => new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.createUser(user);
+      resolve(response);
+    } catch (err) {
+      resolve(err);
     }
+  });
 
-    const updateUserActiveRepo = (id) => {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const response = await db.updateUserActive(id)
-                resolve(response)
-            } catch (err) {
-                resolve(err)
-            }
-        });
+  const updateUserActiveRepo = (id) => new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.updateUserActive(id);
+      resolve(response);
+    } catch (err) {
+      resolve(err);
     }
+  });
 
-    const updateUserPasswordRepo = (id, password) => {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const response = await db.updateUserPassword(id, password)
-                resolve(response)
-            } catch (err) {
-                resolve(err)
-            }
-        });
+  const updateUserPasswordRepo = (id, password) => new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.updateUserPassword(id, password);
+      resolve(response);
+    } catch (err) {
+      resolve(err);
     }
+  });
 
-    const findUserByEmailRepo = (email) => {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const response = await db.findUserByEmail(email);
-                resolve(response);
-            } catch (err) {
-                resolve(err);
-            }
-        });
+  const findUserByEmailRepo = (email) => new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.findUserByEmail(email);
+      resolve(response);
+    } catch (err) {
+      resolve(err);
     }
+  });
 
-    const findUserByIdRepo = (id) => {
-        return new Promise( async (resolve, reject) => {
-            try {
-                const response = await db.findUserById(id)
-                resolve(response)
-            } catch (err) {
-                resolve(err)
-            }
-        });
+  const findUserByIdRepo = (id) => new Promise(async (resolve, reject) => {
+    try {
+      const response = await db.findUserById(id);
+      resolve(response);
+    } catch (err) {
+      resolve(err);
     }
+  });
 
-    return Object.create({
-        findUserByEmailRepo,
-        findUserByIdRepo,
-        createUserRepo,
-        updateUserPasswordRepo,
-        updateUserActiveRepo
-    });
-}
+  return Object.create({
+    findUserByEmailRepo,
+    findUserByIdRepo,
+    createUserRepo,
+    updateUserPasswordRepo,
+    updateUserActiveRepo,
+  });
+};
 
 export default repository();
-
