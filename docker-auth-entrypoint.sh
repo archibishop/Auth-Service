9899f231af406b2c2d8a7405fbd530f09f97d701
Wagubi Brian <36948493+archibishop@users.noreply.github.com>
2019-11-21 14:10:35 +0300

    Making changes for app to run on AWS EC2. (#18)

--- docker-auth-entrypoint.sh
+++ docker-auth-entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 echo "starting server."
+echo "Waiting for Db to be ready, On AWS this is required."
+while !</dev/tcp/db/5432;
+do
+    echo "Database not ready to recieve connections"
+    sleep 1;
+done;
 echo "Run migrations."
 node_modules/.bin/sequelize db:create
 node_modules/.bin/sequelize db:migrate
